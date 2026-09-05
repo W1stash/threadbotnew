a80a75fda1c2da053138263414ac7abc17897e3b
W1stash <154755772+W1stash@users.noreply.github.com>
2026-09-05 19:17:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ async def calculate(update, context):
 
     try:
         yen = float(text.replace(",", "."))
-        rub = int((yen * 0.62) + 800) // 100 * 100  # целое число
+        rub = int((yen * 0.67) + 1488) // 100 * 100  # целое число
 
         await update.message.reply_text(str(rub))
 
